4276269447c32463f77543d51670cb57a3d5540c
Kim Byeonghoon <byounghun02@gmail.com>
2026-05-16 19:51:10 +0900

    upgrade spam checker with MLFlow

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
 import os
+
 MODEL_MODE = "ml" # "rules"
-LOCAL_MODEL_PATH = "ml/artifacts/spam_model.joblib" 
+# LOCAL_MODEL_PATH = "ml/artifacts/spam_model.joblib"
+MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
+# MODEL_URI = "runs:/f40bca671bc64b77b95fdc89cb320aff/model"
+MODEL_URI = "models:/spam-model@champion"
